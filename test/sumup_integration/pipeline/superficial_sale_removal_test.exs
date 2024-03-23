3f12931b4b8539179c2200edc52acf896537fdd4
German Öö <german.oo@hey.com>
2024-03-23 17:48:23 +0200

    test: add test for superficial_sale_removal

---
+++ test/sumup_integration/pipeline/superficial_sale_removal_test.exs
@@ -0,0 +1,32 @@
+defmodule SumupIntegration.Pipeline.SuperficialSaleRemovalTest do
+  use ExUnit.Case
+
+  import SumupIntegration.Factory
+
+  alias SumupIntegration.Pipeline.SuperficialSaleRemoval
+  alias SumupIntegration.Sales.SaleTransaction
+
+  describe "run/1" do
+    test "overwrites amount when it's superificial" do
+      transactionA = build(:sale_transaction)
+      transactionB = build(:sale_transaction, amount: 0.03, quantity: 3)
+      transcationC = build(:sale_transaction, amount: 0.01, quantity: 1)
+
+      assert [
+               ^transactionA,
+               %SaleTransaction{amount: 0, quantity: 3},
+               %SaleTransaction{amount: 0, quantity: 1}
+             ] = SuperficialSaleRemoval.run([transactionA, transactionB, transcationC])
+    end
+
+    test "leaves unmatched transactions as-is and preserves order" do
+      transactions = [
+        build(:sale_transaction),
+        build(:sale_transaction),
+        build(:sale_transaction)
+      ]
+
+      assert ^transactions = SuperficialSaleRemoval.run(transactions)
+    end
+  end
+end
