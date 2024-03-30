c781ad079e1acd287d4355655fba8a1e6357689f
German Öö <german.oo@hey.com>
2024-03-30 14:11:11 +0200

    fix: make sure github config has correct path

--- lib/sumup_integration.ex
+++ lib/sumup_integration.ex
@@ -1,18 +1,5 @@
 defmodule SumupIntegration do
   @moduledoc """
-  Documentation for `SumupIntegration`.
+  A root module for `SumupIntegration`
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SumupIntegration.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
